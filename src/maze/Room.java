e7821a61134be30b1f51ef80bbb93dcd3cec8eaf
ArthurBazoyan <arthur.bazoyan@gmail.com>
2019-12-04 03:49:28 +0400

    Finished the Merging function with the existence of rooms, it now deletes the dead end if it cannot open a hole to another path.

--- src/maze/Room.java
+++ src/maze/Room.java
@@ -10,14 +10,14 @@ public class Room
     {
         int maze_width = maze[0].length;  
         int maze_height = maze.length;   
-        int scale = 5;
+        int scale = 3;
 
         int max_height = maze_height / scale;   // setting the max limit for the height
         int max_width = maze_width   / scale;     // setting the max limit for the width
 
         // randomly selecting a number in between min and maximum width/height
-        width = get_random(2, max_height );
-        height = get_random(2, max_width );
+        width = get_random(1, max_height );
+        height = get_random(1, max_width );
 
     }
 
